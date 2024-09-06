9e28632fd7ce4ea8998008b1c7c2d15990a987a3
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-09-06 09:32:44 -0600

    subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,22 @@
+'''diego ali
+trabajando con cadenas.
+'''
+nombre="Diego"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["E","L","I"]
+for chicas in MisNovias:
+    print(chicas)
+
+    print("otro ejemplo")
+    b = "diego ali"
+    print(b[2:5])
+
+    print(" a mayusculas")
+    a = "Diego ali"
+print(a.upper())
+a = "Maria"
+b = " Perez"
+c = a +"_"+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,19 @@
+import math
+edad=65
+
+nombre="Diego Ledezma"
+print("tu edad es ",edad)
+print("te llamas ",nombre)
+print("-- tipo de variables --")
+x = str(7)
+y = int(3.1416)
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--- funciones matematicas ----")
+import math
+
+x = math.sqrt(64)
+
+print(x)
